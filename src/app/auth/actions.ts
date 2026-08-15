f27913aad3a9e74b2bd9bbd16736bafb7f058205
Popoola Mubarak <Mubarakayomide125@gmail.com>
2026-08-15 20:58:56 +0100

    Change of error

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -26,10 +26,7 @@ export async function signIn(formData: FormData) {
 export async function signUp(formData: FormData) {
   const supabase = await createClient();
 
-<<<<<<< HEAD
-=======
   const name = formData.get("name") as string;
->>>>>>> main
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
@@ -37,28 +34,18 @@ export async function signUp(formData: FormData) {
     email,
     password,
     options: {
-<<<<<<< HEAD
-=======
       data: {
         name,
       },
->>>>>>> main
       emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/confirm`,
     },
   });
 
   if (error) {
-<<<<<<< HEAD
-    redirect(`/sign-up?error=${encodeURIComponent(error.message)}`);
-  }
-
-  redirect("/sign-up?message=Check%20your%20email%20to%20confirm%20your%20account");
-=======
     redirect(`/signup?error=${encodeURIComponent(error.message)}`);
   }
 
   redirect(
     "/signup?message=Check%20your%20email%20to%20confirm%20your%20account"
   );
->>>>>>> main
 }
