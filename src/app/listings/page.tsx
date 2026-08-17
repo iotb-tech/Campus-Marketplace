dc7f6ba66fcca0747494e32368949e7c325be593
nikinggs <neyoboy.aa@gmail.com>
2026-08-18 00:39:05 +0100

    added wrap fragment on root page

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -31,6 +31,7 @@ export default async function ListingsPage() {
 
   // Render successfully loaded listings page
   return (
+    <>
     <div className="min-h-screen bg-gray-50 flex flex-col justify-between">
       <Nav />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 w-full grow">
@@ -45,5 +46,6 @@ export default async function ListingsPage() {
       </main>
       <Footer />
     </div>
+    </>
   );
 }
